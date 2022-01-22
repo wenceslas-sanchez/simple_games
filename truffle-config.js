edd3d55fae88941c7614f8d64acd505bafc5978d
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-01-22 18:12:32 +0100

    🎉 setup truffle project

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*", // match any network
+    },
+    develop: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*", // match any network
+    },
+    mynetwork: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*", // match any network
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.11",
+    },
+  },
+};
