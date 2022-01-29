03916b06b6ebfa46300634d099b88a94035b4b70
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-01-29 21:16:44 +0100

    🎨 add invitation pending

--- contracts/PlayGame.sol
+++ contracts/PlayGame.sol
@@ -5,10 +5,13 @@ import {TicTacToe} from "./TicTacToe.sol";
 contract GameFactory {
     string[] possibleGames = ["tictactoe"];
 
-    function isGameExist(string _name) external returns (bool) {
+    function isGameExist(string calldata _name) external returns (bool) {
         bool result = false;
         for (uint8 i = 0; i < possibleGames.length; i++) {
-            if (_name == possibleGames[i]) {
+            if (
+                keccak256(abi.encodePacked(_name)) ==
+                keccak256(abi.encodePacked(possibleGames[i]))
+            ) {
                 result = true;
                 break;
             }
@@ -16,15 +19,16 @@ contract GameFactory {
         return result;
     }
 
-    function buildGame(
-        string _name,
-        address _player1,
-        address _player2
-    ) external returns (address) {
+    function buildGame(string calldata _name, address _player2)
+        external
+        returns (address)
+    {
         // TODO default, to do in assembly
-        if (_name == "tictactoe") {
-            // TODO test it doesnt create a new contract each time its called
-            return address(new TicTacToe(_player1, _player2));
+        if (
+            keccak256(abi.encodePacked(_name)) ==
+            keccak256(abi.encodePacked("tictactoe"))
+        ) {
+            return address(new TicTacToe(msg.sender, _player2));
         } else {
             return address(0);
         }
@@ -33,34 +37,42 @@ contract GameFactory {
 
 contract PlayGame {
     GameFactory gameFactory;
-    mapping(string => mapping(address => address)) public pendingInvitation;
+    mapping(string => mapping(address => address)) public pendingInvitations;
+    mapping(bytes => address) public pendingGames;
 
-    event InvitationAccepted(string _name, address _player1);
+    event SendInvitation(string _name, address _from, address _to);
+    event AcceptInvitation(string _name, address _from);
 
-    modifier isInvited(address _player1) {
+    modifier isInvited(string calldata _name, address _from) {
         require(
-            pendingInvitation[_player1] == msg.sender,
+            pendingInvitations[_name][_from] == msg.sender,
             "You are not invited by this player."
         );
         _;
     }
 
-    modifier isGameExist(string _name) {
+    modifier isGameExist(string calldata _name) {
         bool result = gameFactory.isGameExist(_name);
         require(result, "This game is not defined.");
         _;
     }
 
-    function startGame(string _name) external isGameExist(_name) {}
-
-    function acceptInvitation(string _name, address _player1)
+    function invitePlayer(string calldata _name, address _to)
         external
         isGameExist(_name)
-        isInvited(_player1)
     {
-        delete pendingInvitation[_name][_player1];
-        emit InvitationAccepted(_name, _player1);
+        pendingInvitation[_name][msg.sender] = _to;
+        emit SendInvitation(_name, msg.sender, _to);
     }
 
-    function _instanceGame(string _name) private {}
+    function instanceGame(string calldata _name) external isGameExist(_name) {}
+
+    function acceptInvitation(string calldata _name, address _from)
+        external
+        isGameExist(_name)
+        isInvited(_name, _from)
+    {
+        delete pendingInvitations[_name][_from];
+        emit AcceptInvitation(_name, _from);
+    }
 }
