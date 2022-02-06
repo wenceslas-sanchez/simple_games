a6b61b965778d0309ad43b104dbff3c54600f68b
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-02-06 19:38:56 +0100

    ✨ add a GameFactory contract, to generate game contract addresses

---
+++ contracts/GameFactory.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.0;
+
+import {TicTacToe} from "./TicTacToe.sol";
+
+contract GameFactory {
+    string[] possibleGames = ["tictactoe"];
+
+    function isGameExist(string calldata _name) external returns (bool) {
+        bool result = false;
+        for (uint8 i = 0; i < possibleGames.length; i++) {
+            if (
+                keccak256(abi.encodePacked(_name)) ==
+                keccak256(abi.encodePacked(possibleGames[i]))
+            ) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    function buildGame(string calldata _name, address _player1)
+    external
+    returns (address)
+    {
+        // TODO default, to do in assembly
+        if (
+            keccak256(abi.encodePacked(_name)) ==
+            keccak256(abi.encodePacked("tictactoe"))
+        ) {
+            return address(new TicTacToe(_player1, msg.sender));
+        } else {
+            return address(0);
+        }
+    }
+}
