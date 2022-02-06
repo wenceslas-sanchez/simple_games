c54d0504303e6c008ef612737a5e06d1329a4213
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-02-06 19:37:53 +0100

    ✨ add events and and arguments to abstract Game contract

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -4,6 +4,14 @@ abstract contract Game {
     struct GameInstance {
         address player1;
         address player2;
+        address winner;
+        bool isGameFinished;
         uint8 turn;
+
     }
+    event PlayTurn(address player);
+    event GameIsWon(address game, address player);
+    event ExAequo(address game, address player1, address player2);
+
+    function play() public {}
 }
