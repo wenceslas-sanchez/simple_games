10c206174a456484a90ef9afd4e66646aab799c5
autoprettier <actions@github.com>
2022-03-05 17:41:37 +0000

    🎨 Format Solidity code with Prettier

--- contracts/LinkedListAddress.sol
+++ contracts/LinkedListAddress.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.0;
 
 contract LinkedListBytes32 {
     bytes32 public head;
-    uint public __length= 0;
+    uint256 public __length = 0;
 
     struct Object {
         bytes32 next;
@@ -16,38 +16,40 @@ contract LinkedListBytes32 {
     event Removed(bytes32 key);
 
     function append(bytes32 _value, bytes32 _key) public {
-        Object memory _obj= Object(head, _key, _value);
-        bytes32 _id= keccak256(abi.encodePacked(head, _key, _value, __length));
-        objects[_id]= _obj;
-        head= id;
+        Object memory _obj = Object(head, _key, _value);
+        bytes32 _id = keccak256(abi.encodePacked(head, _key, _value, __length));
+        objects[_id] = _obj;
+        head = id;
         __length++;
 
         emit Appended(head, _obj.next, _obj.key, _obj.value);
     }
 
-    function length() public view returns (uint) {
-
+    function length() public view returns (uint256) {
         return __length;
     }
 
     function remove(bytes32 _key) public {
-        bytes32 _head= head;
+        bytes32 _head = head;
         bool _r;
-        for (uint i= 0; i<= length; i++) {
-            Object memory _obj= objects[_head];
-            (_r, _next)= _remove_obj(_obj, _key);
+        for (uint256 i = 0; i <= length; i++) {
+            Object memory _obj = objects[_head];
+            (_r, _next) = _remove_obj(_obj, _key);
             if (_r) {
                 if (i == 0) {
-                    head= _next;
+                    head = _next;
                 }
                 break;
             }
         }
     }
 
-    function _remove_obj(bytes32 _head, bytes32 _key) private returns (bool, bytes32) {
-        Object memory _obj= objects[_head];
-        bytes32 _next= obj.next;
+    function _remove_obj(bytes32 _head, bytes32 _key)
+        private
+        returns (bool, bytes32)
+    {
+        Object memory _obj = objects[_head];
+        bytes32 _next = obj.next;
         if (_obj.key == _key) {
             delete objects[_head];
             length--;
