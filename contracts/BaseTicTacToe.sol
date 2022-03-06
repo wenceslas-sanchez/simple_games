9e12dface31b15a40bc73d29d0b3463d0c585db6
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-03-06 21:01:34 +0100

    🎨 split TicTacToe and save pure data into BaseTicTacToe

---
+++ contracts/BaseTicTacToe.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.8.0;
+
+import {Utils} from "./Utils.sol";
+import {Game} from "./Game.sol";
+
+contract BaseTicTacToe is Game, Utils {
+    uint8 constant frameSize = 3;
+    uint8[2] playerNumber = [uint8(1), uint8(4)];
+    uint8[2] playerScoreWin = [uint8(3), uint8(12)];
+    struct TTTGameInstance {
+        GameInstance gameInstance;
+        uint8[frameSize][frameSize] frame;
+        uint8 numMove;
+    }
+
+    TTTGameInstance game;
+
+    function _checkWinner(
+        uint8[frameSize][frameSize] memory _frame,
+        uint8 _player
+    ) internal view returns (bool) {
+        uint8 h;
+        uint8 v;
+        uint8 rd_i;
+        uint8 ld = 0;
+        uint8 rd = 0;
+        for (uint8 i = 0; i < frameSize; i++) {
+            h = 0;
+            v = 0;
+            rd_i = _abs8(int8(i) - int8(frameSize) + 1);
+            for (uint8 j = 0; j < frameSize; j++) {
+                // check horizontal
+                h += _frame[i][j];
+                // check vertical
+                v += _frame[j][i];
+            }
+            if (h == playerScoreWin[_player] || v == playerScoreWin[_player]) {
+                return true;
+            }
+            // check left diagonal
+            ld += _frame[i][i];
+            // check right diagonal
+            rd += _frame[i][rd_i];
+
+            if (
+                ld == playerScoreWin[_player] || rd == playerScoreWin[_player]
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,17 +1,9 @@
 pragma solidity ^0.8.0;
 
-import {Utils} from "./Utils.sol";
-import {Game} from "./Game.sol";
+import {BaseTicTacToe} from "./BaseTicTacToe.sol";
 
-contract TicTacToe is Game, Utils {
-    uint8 constant frameSize = 3;
-    uint8[2] playerNumber = [uint8(1), uint8(4)];
-    uint8[2] playerScoreWin = [uint8(3), uint8(12)];
-    struct TTTGameInstance {
-        GameInstance gameInstance;
-        uint8[frameSize][frameSize] frame;
-        uint8 numMove;
-    }
+
+contract TicTacToe is BaseTicTacToe {
     TTTGameInstance game;
 
     constructor(address _player1, address _player2) {
@@ -69,10 +61,10 @@ contract TicTacToe is Game, Utils {
     }
 
     function _actionFrame(uint8[2] memory _coord)
-        internal
-        cellAlreadyPlayed(_coord)
-        isPlayerTurn
-        returns (bool)
+    internal
+    cellAlreadyPlayed(_coord)
+    isPlayerTurn
+    returns (bool)
     {
         bool _isWinner = false;
         bool _isplayer1 = game.gameInstance.turn == 0;
@@ -85,41 +77,5 @@ contract TicTacToe is Game, Utils {
         }
 
         return _isWinner;
-    }
-
-    function _checkWinner(
-        uint8[frameSize][frameSize] memory _frame,
-        uint8 _player
-    ) internal view returns (bool) {
-        uint8 h;
-        uint8 v;
-        uint8 rd_i;
-        uint8 ld = 0;
-        uint8 rd = 0;
-        for (uint8 i = 0; i < frameSize; i++) {
-            h = 0;
-            v = 0;
-            rd_i = _abs8(int8(i) - int8(frameSize) + 1);
-            for (uint8 j = 0; j < frameSize; j++) {
-                // check horizontal
-                h += _frame[i][j];
-                // check vertical
-                v += _frame[j][i];
-            }
-            if (h == playerScoreWin[_player] || v == playerScoreWin[_player]) {
-                return true;
-            }
-            // check left diagonal
-            ld += _frame[i][i];
-            // check right diagonal
-            rd += _frame[i][rd_i];
-
-            if (
-                ld == playerScoreWin[_player] || rd == playerScoreWin[_player]
-            ) {
-                return true;
-            }
-        }
-        return false;
     }
 }
