6f195e1d3d399d9f16634f8fac6ffd4224c08b64
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-03-06 18:20:25 +0100

    🐛 forget to instantiate contracts ... I'm a idiot

--- contracts/PlayGame.sol
+++ contracts/PlayGame.sol
@@ -6,9 +6,9 @@ import {LinkedListBytes32} from "./LinkedListBytes32.sol";
 
 contract PlayGame is Utils {
 
-    GameFactory gameFactory;
-    LinkedListBytes32 pendingInvitations;
-    LinkedListBytes32 gameInstantiated;
+    GameFactory gameFactory= new GameFactory();
+    LinkedListBytes32 pendingInvitations= new LinkedListBytes32();
+    LinkedListBytes32 gameInstantiated= new LinkedListBytes32();
 
     event SendInvitation(string _name, address _from, address _to);
     event AcceptInvitation(string _name, address _from, address _to, address _game);
@@ -20,35 +20,36 @@ contract PlayGame is Utils {
     }
 
     modifier isGameExist(string calldata _name) {
-        require(gameFactory.isGameExist(_name), "This game is not defined.");
+        bool _r= gameFactory.isGameExist(_name);
+        require(_r, "This game is not defined.");
         _;
     }
 
     function invitePlayer(string calldata _name, address _to)
-        public
-        isGameExist(_name)
+    public
+    isGameExist(_name)
     {
         pendingInvitations.append(addressToBytes32(msg.sender), addressToBytes32(_to));
         emit SendInvitation(_name, msg.sender, _to);
     }
 
     function acceptInvitation(string calldata _name, address _from)
-        external
-        isGameExist(_name)
-        isInvited(_from)
-        returns (address _game)
+    external
+    isGameExist(_name)
+    isInvited(_from)
+    returns (address _game)
     {
-        pendingInvitations.remove(_from);
+        pendingInvitations.remove(addressToBytes32(_from));
         _game= gameFactory.buildGame(_name, _from);
         gameInstantiated.append(_hashBothAddresses(_from, msg.sender), addressToBytes32(_game));
         emit AcceptInvitation(_name, _from, msg.sender, _game);
     }
 
-    function getPendingInvitation(address _from) public view returns (address) {
+    function getPendingInvitation(address _from) public returns (address) {
         return bytes32ToAddress(pendingInvitations.get(addressToBytes32(_from)));
     }
 
-    function getGameInstantiated(address _from, address _to) public view returns (address) {
-        return bytes32ToAddress(gameInstantiated(_hashBothAddresses(_from, _to)));
+    function getGameInstantiated(address _from, address _to) public returns (address) {
+        return bytes32ToAddress(gameInstantiated.get(_hashBothAddresses(_from, _to)));
     }
 }
