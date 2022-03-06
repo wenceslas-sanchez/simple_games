63e4baf0a473d28117049e9c405efc3b6168e902
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-03-06 16:51:30 +0100

    ✨ improve usage of linked list + add legacy

--- contracts/LinkedListBytes32.sol
+++ contracts/LinkedListBytes32.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.8.0;
 
-contract LinkedListBytes32 {
-    bytes32 public head;
-    uint256 public __length = 0;
+import {ILinkedList} from "./ILinkedList.sol";
+
+contract LinkedListBytes32 is ILinkedList {
 
     struct Object {
         bytes32 next;
@@ -13,28 +13,38 @@ contract LinkedListBytes32 {
     mapping(bytes32 => Object) objects;
 
     event Append(bytes32 head, bytes32 next, bytes32 key, bytes32 value);
-    event Remove(bytes32 key);
-    event Get(bytes32 _key);
-    event MissingElement(bytes32 _key);
 
-    function append(bytes32 _value, bytes32 _key) public {
+    function checkKeyNotExist(bytes32 _key) internal view returns (bool) {
+        bytes32 _head = head;
+        for (uint256 i = 0; i <= __length; i++) {
+            Object memory _obj = objects[_head];
+            if (_obj.key == _key) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    modifier isKeyExist(bytes32 _key) {
+        require(checkKeyNotExist(_key), "Key already implemented.");
+        _;
+    }
+
+    function append(bytes32 _key, bytes32 _value) public isKeyExist(_key) {
         Object memory _obj = Object(head, _key, _value);
         bytes32 _id = keccak256(abi.encodePacked(head, _key, _value, __length));
         objects[_id] = _obj;
-        head = id;
+        head = _id;
         __length++;
 
         emit Append(head, _obj.next, _obj.key, _obj.value);
     }
 
-    function length() public view returns (uint256) {
-        return __length;
-    }
-
-    function remove(bytes32 _key) public {
+    function remove(bytes32 _key) override public {
         bytes32 _head = head;
+        bytes32 _next;
         bool _r;
-        for (uint256 i = 0; i <= length; i++) {
+        for (uint256 i = 0; i <= __length; i++) {
             (_r, _next) = _remove_obj(_head, _key);
             if (_r) {
                 if (i == 0) {
@@ -50,14 +60,14 @@ contract LinkedListBytes32 {
     }
 
     function _remove_obj(bytes32 _head, bytes32 _key)
-        private
-        returns (bool, bytes32)
+    private
+    returns (bool, bytes32)
     {
         Object memory _obj = objects[_head];
-        bytes32 _next = obj.next;
+        bytes32 _next = _obj.next;
         if (_obj.key == _key) {
             delete objects[_head];
-            length--;
+            __length--;
             emit Remove(_key);
 
             return (true, _next);
@@ -66,11 +76,10 @@ contract LinkedListBytes32 {
         return (false, _next);
     }
 
-    function get(bytes32 _key) public view returns (bytes32) {
+    function get(bytes32 _key) public returns (bytes32) {
         bytes32 _head = head;
         bytes32 _value;
-        bool _r;
-        for (uint256 i = 0; i <= length; i++) {
+        for (uint256 i = 0; i <= __length; i++) {
             Object memory _obj = objects[_head];
             if (_obj.key == _key) {
                 emit Get(_key);
@@ -78,24 +87,7 @@ contract LinkedListBytes32 {
             }
             _head = _obj.next;
         }
-        if (_value == 0) {
-            emit MissingElement(_key);
-        }
 
         return _value;
-    }
-
-    function _get_obj(
-        bytes32 _head,
-        bytes32 _next,
-        bytes32 _value
-    ) private returns (bytes32) {
-        Object memory _obj = objects[_head];
-        bytes32 _next = obj.next;
-        if (_obj.key == _key) {
-            emit Get(_key);
-            return _obj.value;
-        }
-        return;
     }
 }
