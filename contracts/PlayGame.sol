b4cc571c2ad8ebf4cb524e4475f3cd215cbffa8b
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-03-06 17:35:12 +0100

    🚧 change data structure for invitations and game instances

--- contracts/PlayGame.sol
+++ contracts/PlayGame.sol
@@ -2,51 +2,53 @@ pragma solidity ^0.8.0;
 
 import {GameFactory} from "./GameFactory.sol";
 import {Utils} from "./Utils.sol";
+import {LinkedListBytes32} from "./LinkedListBytes32.sol";
 
 contract PlayGame is Utils {
+
     GameFactory gameFactory;
-    mapping(string => mapping(address => address)) public pendingInvitations;
-    mapping(string => mapping(bytes32 => address)) public GameInstantiated;
+    LinkedListBytes32 pendingInvitations;
+    LinkedListBytes32 gameInstantiated;
 
     event SendInvitation(string _name, address _from, address _to);
-    event AcceptInvitation(string _name, address _from, address _to);
+    event AcceptInvitation(string _name, address _from, address _to, address _game);
 
-    modifier isInvited(string calldata _name, address _from) {
-        require(
-            pendingInvitations[_name][_from] == msg.sender,
-            "You are not invited by this player."
-        );
+    modifier isInvited(address _from) {
+        bytes32 _to= pendingInvitations.get(addressToBytes32(_from));
+        require(!(_to == bytes32(0)), "You are not invited in a game.");
         _;
     }
 
     modifier isGameExist(string calldata _name) {
-        bool result = gameFactory.isGameExist(_name);
-        require(result, "This game is not defined.");
+        require(gameFactory.isGameExist(_name), "This game is not defined.");
         _;
     }
 
     function invitePlayer(string calldata _name, address _to)
-        external
+        public
         isGameExist(_name)
     {
-        pendingInvitations[_name][msg.sender] = _to;
+        pendingInvitations.append(addressToBytes32(msg.sender), addressToBytes32(_to));
         emit SendInvitation(_name, msg.sender, _to);
     }
 
     function acceptInvitation(string calldata _name, address _from)
         external
         isGameExist(_name)
-        isInvited(_name, _from)
+        isInvited(_from)
+        returns (address _game)
     {
-        bytes32 hash = _hashBothAddresses(
-            pendingInvitations[_name][_from],
-            msg.sender
-        );
-        GameInstantiated[_name][hash] = gameFactory.buildGame(
-            _name,
-            pendingInvitations[_name][_from]
-        );
-        delete pendingInvitations[_name][_from];
-        emit AcceptInvitation(_name, _from, msg.sender);
+        pendingInvitations.remove(_from);
+        _game= gameFactory.buildGame(_name, _from);
+        gameInstantiated.append(_hashBothAddresses(_from, msg.sender), addressToBytes32(_game));
+        emit AcceptInvitation(_name, _from, msg.sender, _game);
+    }
+
+    function getPendingInvitation(address _from) public view returns (address) {
+        return bytes32ToAddress(pendingInvitations.get(addressToBytes32(_from)));
+    }
+
+    function getGameInstantiated(address _from, address _to) public view returns (address) {
+        return bytes32ToAddress(gameInstantiated(_hashBothAddresses(_from, _to)));
     }
 }
